c2c56f3698faa6dcfc220ffcba52e2df201b9392
hexoul <crosien@gmail.com>
2018-10-28 14:11:02 +0900

    refactor: reduce main by making steal package

--- main.go
+++ main.go
@@ -1,69 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net"
 	"os"
 	"strconv"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common"
-
-	"github.com/hexoul/ether-stealer/contract"
 	"github.com/hexoul/ether-stealer/crypto"
-	"github.com/hexoul/ether-stealer/json"
 	"github.com/hexoul/ether-stealer/log"
+	"github.com/hexoul/ether-stealer/steal"
 
 	"github.com/korovkin/limiter"
-)
-
-const (
-	urlForBalance = "https://api.infura.io/v1/jsonrpc/mainnet/eth_getBalance?params=[\"%s\",\"latest\"]"
-	urlForTxCount = "https://api.infura.io/v1/jsonrpc/mainnet/eth_getTransactionCount?params=[\"%s\",\"latest\"]"
 )
 
 var (
 	nLimit int
 	who    string
 )
-
-func get(baseURL, addr string) (b bool, val string) {
-	url := fmt.Sprintf(baseURL, addr)
-	ret, err := json.GetRPCResponseFromURL(url)
-	if err != nil {
-		fmt.Print("UNSTABLE NETWORK")
-		return
-	}
-
-	switch ret.Result.(type) {
-	case string:
-		if val = ret.Result.(string); val != "0x0" {
-			b = true
-		}
-	}
-	return
-}
-
-func hasBalance(addr string) (b bool, val string) {
-	return get(urlForBalance, addr)
-}
-
-func hasTxCount(addr string) (b bool, val string) {
-	return get(urlForTxCount, addr)
-}
-
-func steal(addr common.Address, privkey []byte) {
-	addrStr := addr.String()
-	if canStealEther, _ := hasBalance(addrStr); canStealEther {
-		log.Infof("STEAL ETHER from %s !!SECRET!! %x", addrStr, privkey)
-	} else if canCandidate, _ := hasTxCount(addrStr); canCandidate {
-		log.Infof("GOT CANDIDATE %s !!SECRET!! %x", addrStr, privkey)
-	} else if canStealERC := contract.CanSteal(addr); canStealERC != "" {
-		log.Infof("STEAL ERC20 from %s !!SECRET!! %x TARGET %s", addrStr, privkey, canStealERC)
-	} else {
-		fmt.Printf("FAILED from %s\n", addrStr)
-	}
-}
 
 func init() {
 	nLimit = 10
@@ -100,6 +53,6 @@ func main() {
 	for {
 		pubkey, privkey := crypto.GenerateKeyPair()
 		addr := crypto.ToAddressFromPubkey(pubkey)
-		limit.Execute(func() { steal(addr, privkey) })
+		limit.Execute(func() { steal.Steal(addr, privkey) })
 	}
 }
---
+++ steal/steal.go
@@ -0,0 +1,53 @@
+package steal
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/hexoul/ether-stealer/contract"
+	"github.com/hexoul/ether-stealer/json"
+	"github.com/hexoul/ether-stealer/log"
+)
+
+const (
+	urlForBalance = "https://api.infura.io/v1/jsonrpc/mainnet/eth_getBalance?params=[\"%s\",\"latest\"]"
+	urlForTxCount = "https://api.infura.io/v1/jsonrpc/mainnet/eth_getTransactionCount?params=[\"%s\",\"latest\"]"
+)
+
+func get(baseURL, addr string) (b bool, val string) {
+	url := fmt.Sprintf(baseURL, addr)
+	ret, err := json.GetRPCResponseFromURL(url)
+	if err != nil {
+		fmt.Println("UNSTABLE NETWORK")
+		return
+	}
+
+	switch ret.Result.(type) {
+	case string:
+		if val = ret.Result.(string); val != "0x0" {
+			b = true
+		}
+	}
+	return
+}
+
+func hasBalance(addr string) (b bool, val string) {
+	return get(urlForBalance, addr)
+}
+
+func hasTxCount(addr string) (b bool, val string) {
+	return get(urlForTxCount, addr)
+}
+
+func Steal(addr common.Address, privkey []byte) {
+	addrStr := addr.String()
+	if canStealEther, _ := hasBalance(addrStr); canStealEther {
+		log.Infof("STEAL ETHER from %s !!SECRET!! %x", addrStr, privkey)
+	} else if canCandidate, _ := hasTxCount(addrStr); canCandidate {
+		log.Infof("GOT CANDIDATE %s !!SECRET!! %x", addrStr, privkey)
+	} else if canStealERC := contract.CanSteal(addr); canStealERC != "" {
+		log.Infof("STEAL ERC20 from %s !!SECRET!! %x TARGET %s", addrStr, privkey, canStealERC)
+	} else {
+		fmt.Printf("FAILED from %s\n", addrStr)
+	}
+}
